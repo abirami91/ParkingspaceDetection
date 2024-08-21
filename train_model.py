61161bfbd777b473542b5e8ec56911487be6f398
Abirami Sridharan <abiramisridharan@gmail.com>
2024-08-21 15:25:27 +0200

    Improve learning of the model and include the app

--- train_model.py
+++ train_model.py
@@ -6,6 +6,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau  # Import the callback
 
 # Define the directory
 directory = 'models'
@@ -13,7 +14,6 @@ directory = 'models'
 # Check if the directory exists, if not, create it
 if not os.path.exists(directory):
     os.makedirs(directory)
-
 
 # Load preprocessed data
 data = np.load('data/preprocessed_data.npz')
@@ -36,23 +36,26 @@ datagen.fit(X_train)
 
 # Use the CNN model(sequential to train the data)
 model = Sequential([
-    tf.keras.layers.Input(shape=(150, 150, 1)),
-    Conv2D(32, (3, 3), activation='relu'),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 1)),
     MaxPooling2D(2, 2),
-    Dropout(0.5),  # Dropout layer added here
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
     Flatten(),
-    Dense(128, activation='relu'),
-    Dropout(0.5),  # Dropout layer added here
+    Dense(256, activation='relu'),
+    Dropout(0.5),  # Add dropout here
     Dense(1, activation='sigmoid')
 ])
 
-# Use an adam optimizer to check the loss and accuracy rate 
+# Use an Adam optimizer to check the loss and accuracy rate 
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
-# Train the model using the augmented data
-model.fit(datagen.flow(X_train, y_train, batch_size=2), epochs=10, validation_data=(X_test, y_test))
+# Define the learning rate scheduler callback
+lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
+
+# Train the model using the augmented data with the learning rate scheduler
+model.fit(datagen.flow(X_train, y_train, batch_size=2), epochs=10, validation_data=(X_test, y_test), callbacks=[lr_scheduler])
 
 # Save the model
 model.save(os.path.join(directory, 'parking_space_model.keras'))
